f2f55019d616fcd22e459569efcef757729201e5
irfanur02 <irfanur820@gmail.com>
2024-06-27 18:14:21 +0700

    admin kelola menu

--- admin/script.js
+++ admin/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-  $("#formTambahMenu [name='selectPack']").on('change', function(e) {
-    alert("pack");
+
+  $("#formMenu select[name='selectPack']").on('change', function() {
+    if(this.value == 1) {
+      $("#formMenu input[name='txtHarga']").attr("disabled", false);
+      $("#formMenu select[name='selectPaket']").attr("disabled", true);
+      $("#formMenu select[name='selectKarbo']").attr("disabled", true);
+    };
+
+    if(this.value == 2) {
+      $("#formMenu input[name='txtHarga']").attr("disabled", true);
+      $("#formMenu select[name='selectPaket']").attr("disabled", false);
+      $("#formMenu select[name='selectKarbo']").attr("disabled", false);
+    };
   });
 
 })
